de462a2d8fddb5ef3b5656796a1e39cc2fa53ead
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-08-11 21:45:14 -0300

    finalizando desafio

--- caixa-da-lanchonete.js
+++ caixa-da-lanchonete.js
@@ -1,3 +1,4 @@
+
 let cardapio = [
     { codigo: 'cafe', descricao: 'Café', valor: 3.00 },
     { codigo: 'chantily', descricao: 'Chantily (extra do Café)', valor: 1.50 },
@@ -8,25 +9,25 @@ let cardapio = [
     { codigo: 'combo1', descricao: '1 Suco e 1 Sanduíche', valor: 9.50 },
     { codigo: 'combo2', descricao: '1 Café e 1 Sanduíche', valor: 7.50 }
 ];
+
 class CaixaDaLanchonete {
-
     calcularValorDaCompra(metodoDePagamento, itens) {
-       
-
-        const formasPagamento = [ 'dinheiro', 'debito', 'credito' ];
-
+        const formasPagamento = [ 'dinheiro', 'debito', 'credito' ];      
         const carrinho = [];
+    
         let valorTotal = 0;
-
+     
         let temCafe = false;
         let temSanduiche = false;
-
+      
         for (const itemStr of itens) {
+          
             const [ codigo, quantidadeStr ] = itemStr.split(',');
+          
             const quantidade = parseInt(quantidadeStr);
 
             const cardapioItem = cardapio.find(i => i.codigo === codigo);
-
+        
             if (!cardapioItem) {
                 return "Item inválido!";
             }
@@ -35,56 +36,50 @@ class CaixaDaLanchonete {
                 return "Quantidade inválida!";
             }
 
+        
             if (codigo === 'cafe') {
                 temCafe = true;
             } else if (codigo === 'sanduiche') {
                 temSanduiche = true;
             }
-
-            carrinho.push({ item: cardapioItem, quantidade: quantidade });
+      
+            carrinho.push({ itemCarrinho: cardapioItem, quantidade: quantidade });
         }
-
-        for (const item of carrinho) {
-            if ((item.item.codigo === 'queijo')  && !temSanduiche) {
+     
+        for (const itemExtra of carrinho) {
+            if ((itemExtra.itemCarrinho.codigo === 'queijo') && !temSanduiche) {
                 return "Item extra não pode ser pedido sem o principal";
             }
         }
-        for (const item of carrinho) {
-            if ((item.item.codigo === 'chantily')  && !temCafe) {
+        for (const itemExtra of carrinho) {
+            if ((itemExtra.itemCarrinho.codigo === 'chantily') && !temCafe) {
                 return "Item extra não pode ser pedido sem o principal";
             }
         }
-        
 
         if (carrinho.length === 0) {
             return "Não há itens no carrinho de compra!";
         }
 
+        
         for (const item of carrinho) {
-            valorTotal += item.item.valor * item.quantidade;
+            valorTotal += item.itemCarrinho.valor * item.quantidade;
         }
-
+      
         if (!formasPagamento.includes(metodoDePagamento)) {
             return "Forma de pagamento inválida!";
         }
 
         if (metodoDePagamento === 'dinheiro') {
-            valorTotal *= 0.95; // Aplicar desconto de 5%
+            valorTotal *= 0.95;
         } else if (metodoDePagamento === 'credito') {
-            valorTotal *= 1.03; // Aplicar acréscimo de 3%
-        } 
+            valorTotal *= 1.03; 
+        }
 
-   
         return `R$ ${valorTotal.toFixed(2).replace('.', ',')}`;
-        
     }
 }
 
-// Exemplos
 const caixa = new CaixaDaLanchonete();
 
-console.log(caixa.calcularValorDaCompra('dinheiro', ['cafe,1'])); // Saída: "R$ 5,25"
-console.log(caixa.calcularValorDaCompra('debito', [ 'cafe,1', 'chantily,1' ])); // Saída: "R$ 4,50"
-console.log(caixa.calcularValorDaCompra('debito', [ 'cafe,1', 'sanduiche,1' ])); // Saída: "R$ 9,23"
-
-export {CaixaDaLanchonete};
+export { CaixaDaLanchonete };
